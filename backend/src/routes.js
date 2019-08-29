a145484e16ae7610e5a7e2045b80787419e22591
EzequielGnich <ezequielgnich@live.com>
2019-08-29 17:34:11 -0300

    Criada as rotas de Vendedores, e clientes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
 
 const UserController = require("./controllers/UserController");
+const SalesmanController = require("./controllers/SalesmanController");
+const ClientController = require("./controllers/ClientController");
 
 const routes = express.Router();
 
 routes.get("/user/:id", UserController.index);
 routes.delete("/user/:id", UserController.delete);
 routes.post("/user", UserController.store);
+
+routes.get("/salesman/:id", SalesmanController.index);
+routes.delete("/salesman/:id", SalesmanController.delete);
+routes.post("/salesman", SalesmanController.store);
+
+routes.get("/client/:id", ClientController.index);
+routes.post("/client", ClientController.store);
+routes.put("/client/:id", ClientController.change);
+routes.delete("/client/:id", ClientController.delete);
 
 module.exports = routes;
